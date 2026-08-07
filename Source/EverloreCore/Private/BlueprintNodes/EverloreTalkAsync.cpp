3e1774f19b9fe5168b5f9c79ae0dd434792ed67c
Simulated Flow <simulatedflow@gmail.com>
2026-08-07 08:26:22 +0200

    publicize: scrub personal data, add license + store links

--- Source/EverloreCore/Private/BlueprintNodes/EverloreTalkAsync.cpp
+++ Source/EverloreCore/Private/BlueprintNodes/EverloreTalkAsync.cpp
@@ -1,4 +1,4 @@
-// Copyright 2026 Silvan Teufel All Rights Reserved.
+// Copyright 2026 Simulated Flow All Rights Reserved.
 
 #include "BlueprintNodes/EverloreTalkAsync.h"
 #include "Components/EverloreConversationComponent.h"
